e0b5eba757022e9e07fc51be758878aa047a233f
Priscila Padilla Latance <priscila.latance@gmail.com>
2024-07-31 13:11:15 -0300

    Inclusão de Script de Banco

---
+++ scriptMySQL/car.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 31/07/2024 às 18:09
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `controlecar`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `car`
+--
+
+CREATE TABLE `car` (
+  `PLACA` varchar(10) NOT NULL,
+  `MARCA` varchar(100) NOT NULL,
+  `MODELO` varchar(150) NOT NULL,
+  `COMBUSTIVEL` varchar(30) NOT NULL,
+  `ANOFABRIC` int(11) NOT NULL,
+  `ANOMODEL` int(11) NOT NULL,
+  `IN_ATIVO` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `car`
+--
+ALTER TABLE `car`
+  ADD PRIMARY KEY (`PLACA`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
